814b9c2e8c4876e59dac4e215ec9206cecf980e8
selfiejesus <slh537@nyu.edu>
2021-11-22 15:34:38 -0500

    Update ledBlink.ino
    fixed syntax error, added push button

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,16 +1,19 @@
 int ledPin = 15;
 int ledPin2 = 14;
+int buttonPin = 22;
 
 void setup() {
+  pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
+if(digitalRead(buttonPin)==HIGH) {
   digitalWrite(ledPin, HIGH);
   digitalWrite(ledPin2, HIGH);
   delay(500);
   digitalWrite(ledPin, LOW);
-  digitalWrite(ledPin2, HIGH);
-  delay(500);
+  digitalWrite(ledPin2, LOW);
+  delay(500); }
 }
