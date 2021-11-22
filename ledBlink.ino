b36c5cb9b65a59bae6ffc712f8d5c348c3270494
selfiejesus <slh537@nyu.edu>
2021-11-22 15:32:14 -0500

    Update ledBlink.ino
    added led2

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,12 +1,16 @@
 int ledPin = 15;
+int ledPin2 = 14;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
   digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin2, HIGH);
   delay(500);
   digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin2, HIGH);
   delay(500);
 }
