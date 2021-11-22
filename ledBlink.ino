0d108e0119c7e1d8294a4f3729293dcb1fa6a90c
selfiejesus <slh537@nyu.edu>
2021-11-22 15:30:11 -0500

    Update ledBlink.ino
    initial code

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,9 +1,12 @@
-void setup() {
-  // put your setup code here, to run once:
+int ledPin = 15;
 
+void setup() {
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
 }
